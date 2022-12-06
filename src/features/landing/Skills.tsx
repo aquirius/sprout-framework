582559ed7191c71fa59364cc75273cc52b3a9771
maximilian@fanbase.com <maximilian@fanbase.com>
2022-12-06 18:36:31 +0100

    add skill images

--- src/features/landing/Skills.tsx
+++ src/features/landing/Skills.tsx
@@ -12,6 +12,15 @@ const StyledProjects = styled.div`
     z-index:1;
 `;
 
+const StyledProjectCoverImage = styled.div<{url: string}>`
+    background-image: url("${props => props.url}");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+    width: 100%;
+`;
+
 const Skills = () : ReactElement => {
   //initialize with empty states
 
@@ -20,13 +29,13 @@ return (
         <StyledProjects>
         <Flexbox direction='row' wrap='wrap'>
           <FlexboxElement align='flex-start' order={1} grow={0}>
-            <Card childFront={<>asdfsadf</>} childBack={<>asfdsa</>}></Card>
+            <Card childFront={<StyledProjectCoverImage url='register-sprout.png'></StyledProjectCoverImage>} childBack={<>asfdsa</>}></Card>
           </FlexboxElement>
           <FlexboxElement align='flex-start' order={1} grow={0}>
-            <Card childFront={<>asdfsadf</>} childBack={<>asfdsa</>}></Card>
+            <Card childFront={<StyledProjectCoverImage url='login-sprout.png'></StyledProjectCoverImage>} childBack={<>asfdsa</>}></Card>
           </FlexboxElement>
           <FlexboxElement align='flex-start' order={1} grow={0}>
-            <Card childFront={<>asdfsadf</>} childBack={<>asfdsa</>}></Card>
+            <Card childFront={<StyledProjectCoverImage url='band-prev.png'></StyledProjectCoverImage>} childBack={<>asfdsa</>}></Card>
           </FlexboxElement>
           <FlexboxElement align='flex-start' order={1} grow={0}>
             <Card childFront={<>asdfsadf</>} childBack={<>asfdsa</>}></Card>
