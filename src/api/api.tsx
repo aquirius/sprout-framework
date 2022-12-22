c7d9a546a8bb31a56b9469ce73907c5678e4330c
maximilian@fanbase.com <maximilian@fanbase.com>
2022-12-22 18:35:38 +0100

    getAPI handler

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,11 +1,11 @@
 import { useCallback, useState } from "react";
 
 export interface APIPost {
-	readonly success: boolean;
-	readonly payload?: any;
-	readonly data?: any;
-	readonly busy: boolean;
-  readonly version: number;
+	readonly postSuccess: boolean;
+	readonly postPayload?: any;
+	readonly postData?: any;
+	readonly postBusy: boolean;
+  readonly postVersion: number;
   readonly post: () => void;
 }
 
@@ -14,28 +14,22 @@ export interface APIRequest{
 }
 
 const useAPIPost = (target: string, method: string, request: APIRequest): APIPost => {
-
-  const [payload, setPayload] = useState<any>()
   const [data, setData] = useState<any>()
   const [success, setSuccess] = useState(false)
   const [busy, setBusy] = useState(true)
   const [versionState, setVersion] = useState(0)
 
-
-  const _request = new Request(target, {
-    method: "post",
-    body: request.payload,
-    headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-        "Method": method
-    }
-  });
-
-  let clone = _request.clone()
-
   var post = useCallback(() => {
-    fetch(clone).then((res) => {
+    const _request = new Request(target, {
+      method: "post",
+      body: request.payload,
+      headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+          "Method": method
+      }
+    });  
+    fetch(_request).then((res) => {
       setVersion(versionState+1)
       if(!res.ok){
         setSuccess(false)
@@ -49,7 +43,7 @@ const useAPIPost = (target: string, method: string, request: APIRequest): APIPos
       setData(json)
       return;
     }).catch(() => undefined)
-  },[versionState])
+  },[versionState, target, method, request.payload])
 
   console.log({
     success: success,
@@ -61,14 +55,70 @@ const useAPIPost = (target: string, method: string, request: APIRequest): APIPos
   })
 
   return {
-    success: success,
-    busy: false,
-    version: versionState,
-    payload: request.payload,
-		data: data,
+    postSuccess: success,
+    postBusy: false,
+    postVersion: versionState,
+    postPayload: request.payload,
+		postData: data,
     post:post,
   }
 }
 
 
-export {useAPIPost}
+
+export interface APIGet {
+	readonly getSuccess: boolean;
+	readonly getData?: any;
+	readonly getBusy: boolean;
+  readonly getVersion: number;
+  readonly get: () => void;
+}
+
+const useAPIGet = (target: string): APIGet => {
+  const [data, setData] = useState<any>()
+  const [success, setSuccess] = useState(false)
+  const [busy, setBusy] = useState(true)
+  const [versionState, setVersion] = useState(0)
+
+  var get = useCallback(() => {
+    const _request = new Request(target, {
+      method: "get",
+      headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+      }
+    });
+    fetch(_request).then((res) => {
+      setVersion(versionState+1)
+      if(!res.ok){
+        setSuccess(false)
+        throw new Error("invalid post");
+      }
+      return res.json()
+    }).then((json)=> {
+      setSuccess(true)
+      setBusy(false)
+      setData(json)
+      return;
+    }).catch(() => undefined)
+  },[versionState, target])
+
+  console.log({
+    getSuccess: success,
+    getBusy: busy,
+    getVersion: versionState,
+		getData: data,
+    get:get,
+  })
+
+  return {
+    getSuccess: success,
+    getBusy: false,
+    getVersion: versionState,
+		getData: data,
+    get:get,
+  }
+}
+
+
+export {useAPIPost, useAPIGet}
--- src/components/Greenhouses.tsx
+++ src/components/Greenhouses.tsx
@@ -1,9 +1,9 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { faBuilding, faPlus } from '@fortawesome/free-solid-svg-icons';
-import React, { ReactElement, useCallback, useEffect, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { useAPIPost } from '../api/api';
+import { useAPIGet, useAPIPost } from '../api/api';
 import { Button } from '../features/button/Button';
 import { Card } from './Card';
 import { Flexbox, FlexboxElement } from './Flexbox';
@@ -17,79 +17,39 @@ interface GreenhousesProps {
   uuid: number
 }
 
-//TODO api interfaces in new folder /api for each system
-interface GreenhouseAPI {
-  GUID: number;
-  Address: string;
-  Zip: number;
-}
-
-interface Greenhouses {
-  message: string
-  greenhouses : GreenhouseAPI[]
-  get: () => void
-}
-
-const useGreenhouses = (uuid : number) => {
-    const [greenhouses, setGreenhouses] = useState<Array<GreenhouseAPI>>()
-    const [message, setMessage] = useState("")
-
-    var get = useCallback(() => {
-      fetch("/user/"+uuid+"/greenhouse")
-      .then((res) => {
-        setMessage(res.statusText)
-        return res.json()})
-      .then((json) => {
-        setGreenhouses(json.greenhouses)
-      })
-  }, [])
-
-    return {
-      message,
-      greenhouses,
-      get:get
-    }
-};
-
 //Button component draws us an html button with icon and size of the icon
 const Greenhouses = ({uuid} : GreenhousesProps) : ReactElement => {
-
-  const [greenhousesState, setGreenhousesState] = useState<any>()
   const [messageFetch, setMessage] = useState("")
-
   const [loading, setLoading] = useState(false)
-  const [guid, setGUID] = useState(0)
 
   const nav = useNavigate();
-  const {message, greenhouses, get} = useGreenhouses(uuid);
-  const {version, success, busy, data, post} = useAPIPost("/user/"+uuid+"/greenhouse", "add", {"payload" : {"UUID": uuid}});
+  const {getData, get} = useAPIGet("/user/"+uuid+"/greenhouse");
+  const {postVersion, postSuccess, postBusy, postData, post} = useAPIPost("/user/"+uuid+"/greenhouse", "add", {"payload" : {"UUID": uuid}});
 
   useEffect(() => {
     get()
-    if (loading || !success || busy || !data){
+    if (loading || !postSuccess || postBusy || !postData){
       return
     }
-    setGreenhousesState(greenhouses)
-    setMessage(message)
+    setMessage("200")
     setLoading(false)
-  }, [version])
+  }, [postVersion])
 
   const onAddGreenhouse = () => {
-    console.log("add")
     post()
     setLoading(true)
   }
 
   return (
-    <>
+    <StyledGreenhouse>
     <Grid layout={"80% 20%"} dimension={"'a b'"} >
       <GridElement position='a'>
         <Flexbox align='center' direction='row' wrap='wrap'>
-        {greenhouses && Object.keys(greenhouses).map((key, index) => {
+        {getData && Object.keys(getData.greenhouses).map((key, index) => {
           return (
             <div key={index}>
               <FlexboxElement align='flex-start' order={0} grow={0}>
-                <Card childFront={<><Button icon={faBuilding as IconProp} onClick={() => nav("/user/"+uuid+"/greenhouse/"+greenhouses[index].GUID)}></Button></>} childBack={<>{greenhouses[index].Address} : {greenhouses[index].Zip}</>}/>
+                <Card childFront={<><Button icon={faBuilding as IconProp} onClick={() => nav("/user/"+uuid+"/greenhouse/"+getData.greenhouses[index].GUID)}></Button></>} childBack={<>{getData.greenhouses[index].Address} : {getData.greenhouses[index].Zip}</>}/>
             </FlexboxElement>
             </div>
           );
@@ -100,10 +60,10 @@ const Greenhouses = ({uuid} : GreenhousesProps) : ReactElement => {
         </Flexbox>
       </GridElement>
       <GridElement position='b'>
-        <Snack danger message={message}></Snack>
+        <Snack danger message={messageFetch}></Snack>
       </GridElement>
     </Grid>
-    </>
+    </StyledGreenhouse>
   );
 }
 
